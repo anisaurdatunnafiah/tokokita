5eb1733f9c1776e99f6ca8f576ca2828cc4e88da
anisaurdatunnafiah <anisaurdatunnafiah@gmail.com>
2024-10-22 00:05:09 +0700

    first

---
+++ src/kelas/product.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package kelas;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class product {
+    int product_id, product_price, product_cat_id;
+    String productt_name, product_desc;
+
+    public int getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(int product_id) {
+        this.product_id = product_id;
+    }
+
+    public int getProduct_price() {
+        return product_price;
+    }
+
+    public void setProduct_price(int product_price) {
+        this.product_price = product_price;
+    }
+
+    public int getProduct_cat_id() {
+        return product_cat_id;
+    }
+
+    public void setProduct_cat_id(int product_cat_id) {
+        this.product_cat_id = product_cat_id;
+    }
+
+    public String getProductt_name() {
+        return productt_name;
+    }
+
+    public void setProductt_name(String productt_name) {
+        this.productt_name = productt_name;
+    }
+
+    public String getProduct_desc() {
+        return product_desc;
+    }
+
+    public void setProduct_desc(String product_desc) {
+        this.product_desc = product_desc;
+    }
+    
+}
